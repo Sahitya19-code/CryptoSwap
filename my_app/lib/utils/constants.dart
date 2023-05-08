917ad7e30a8074771a4368529ea5c4a3e4097593
Sahitya <dolasahitya@gmail.com>
2023-05-08 12:16:34 +0530

    Initial Commit

---
+++ my_app/lib/utils/constants.dart
@@ -0,0 +1,14 @@
+String infura_url =
+    "https://goerli.infura.io/v3/509dfef1ccd844f6ad3f3f2577e47566";
+
+String sepolia_infura_url =
+    "https://sepolia.infura.io/v3/509dfef1ccd844f6ad3f3f2577e47566";
+
+String owner_private_key = " "; //ENTER YOUR PRIVATE KEY
+
+String currentPriceAddress = "0xa629704d23831C1dc811B5d48828BEBd15745385";
+
+String swapTokenAddress =
+    "0xF0722754Ee2f26C9614Ba293D5beB5ec0C8AE919"; //ENTER YOUR TOKENSWAP CONTRACT ADDRESS OR INCREASE ALLOWANCE.
+
+String getBalanceContractAddress = "0xb1f64E85e1762da11067634945fAa83AD5A45689";
